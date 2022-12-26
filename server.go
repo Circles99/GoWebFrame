2458891c07df9deb4c5e50810408e59cbbe162d1
刘吉铭 <liujiming0208@gmail.com>
2022-12-26 22:22:55 +0800

    server init

---
+++ server.go
@@ -0,0 +1,50 @@
+package GoWebFrame
+
+import (
+	"net"
+	"net/http"
+)
+
+
+// 确保一定实现了接口
+var _ Server = &HttpServer{}
+
+type Server interface {
+	http.Handler
+	// Start 启动服务器
+	Start(addr string) error
+}
+
+
+
+
+type HttpServer struct {
+		
+}
+
+func (h *HttpServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	ctx := &Context{
+		Req: request,
+		Resp: writer,
+	}
+	h.serve(ctx)
+}
+
+func (h *HttpServer)serve(ctx *Context) {
+	// 查找路由，执行命中业务逻辑
+}
+
+
+func (h *HttpServer) Start(addr string) error {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	// 可实现用户注册的回调等
+	// 执行一些业务前置操作条件
+	return http.Serve(l, h)
+}
+
+
+
+
