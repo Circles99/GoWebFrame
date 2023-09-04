e309f483380077d032b0dbbc7ac506c0c151ac79
liujiming <907388090@qq.com>
2023-09-04 17:57:52 +0800

    service register init

---
+++ micro/register/types.go
@@ -0,0 +1,27 @@
+package register
+
+import (
+	"context"
+	"io"
+)
+
+type Register interface {
+	// 服务端注册
+	Register(ctx context.Context, si ServiceInstance) error
+	UnRegister(ctx context.Context, si ServiceInstance) error
+	// 客户端
+	ListenServices(ctx context.Context, serviceName string) ([]ServiceInstance, error)
+	Subscribe(serviceName string) (<-chan Event, error)
+
+	io.Closer
+}
+
+type ServiceInstance struct {
+	Name string
+	// Addr 最关键的定位信息
+	Address string
+	// 下面可以任意加字段，完全取决于你的服务治理要做成什么样子
+}
+
+type Event struct {
+}
