b2fa59b27c08ec72fd845c510d56717bc9aca0a7
liujiming <907388090@qq.com>
2023-10-20 15:47:28 +0800

    debug

--- queue/concurrent_blocking_queue.go
+++ queue/concurrent_blocking_queue.go
@@ -10,28 +10,35 @@ type ConcurrentBlockingQueue[T any] struct {
 	data     []T
 	notFull  chan struct{}
 	notEmpty chan struct{}
+	maxSize  int
 }
 
 func NewConcurrentBlockingQueue[T any](maxSize int) *ConcurrentBlockingQueue[T] {
 	m := &sync.Mutex{}
 	return &ConcurrentBlockingQueue[T]{
-		data:  make([]T, 0, maxSize),
-		mutex: m,
-		//notFull:
+		data:     make([]T, 0, maxSize),
+		mutex:    m,
+		notFull:  make(chan struct{}),
+		notEmpty: make(chan struct{}),
+		maxSize:  maxSize,
 	}
 }
 
 func (c *ConcurrentBlockingQueue[T]) Enqueue(ctx context.Context, data any) error {
-	c.mutex.Lock()
-	// 全是bug
-	// 这里需要自己释放锁，如果不是放，另外一边是拿不到
-	select {
-	case <-ctx.Done():
-		c.mutex.Unlock()
+	if ctx.Err() != nil {
 		return ctx.Err()
-	default:
-
 	}
+
+	c.mutex.Lock()
+	//// 全是bug
+	//// 这里需要自己释放锁，如果不是放，另外一边是拿不到
+	//select {
+	//case <-ctx.Done():
+	//	c.mutex.Unlock()
+	//	return ctx.Err()
+	//default:
+	//
+	//}
 
 	// 这里不用if 需要用for
 	// 如果有多个G，比如G1和G2， 同时唤醒，G2直接入队，G1需要再次阻塞自己 所以使用for
@@ -47,29 +54,56 @@ func (c *ConcurrentBlockingQueue[T]) Enqueue(ctx context.Context, data any) erro
 
 	}
 	c.data = append(c.data, data)
-	c.notEmpty <- struct{}{}
+	// 没有人等 notEmpty的新号，这一句会阻塞住
+	if len(c.data) == 1 {
+		// 只有从空变不空发信号
+		c.notEmpty <- struct{}{}
+	}
+
 	c.mutex.Unlock()
 	return nil
 }
 
 func (c *ConcurrentBlockingQueue[T]) Dequeue(ctx context.Context) (any, error) {
+
+	if ctx.Err() != nil {
+		var t T
+		return t, ctx.Err()
+	}
+
 	c.mutex.Lock()
 	for c.IsEmpty() {
 		// 阻塞我自己，等待元素入队
 		c.mutex.Unlock()
 
+		// 进入select之前一定需要释放锁Unlock 不然导致死锁
+		// 阻塞在notEmpty没有关系，上面的Enqueue还能拿到锁，如果不释放，上面的Enqueue拿不到锁
 		select {
 		case <-c.notEmpty:
-
+			c.mutex.Lock()
+		case <-ctx.Done():
+			var t T
+			return t, ctx.Err()
 		}
 
-		//var t T
-		//return t, errors.New("空的队列")
 	}
 
 	// 队首
 	t := c.data[0]
 	c.data = c.data[1:]
+	// 直接使用 c.notFull <- struct{}{}
+	// 如果上面根本没有人在select接收，会永远阻塞在这
+
+	if len(c.data) == c.maxSize-1 {
+		// 只有从满变不满发信号
+		select {
+		case c.notFull <- struct{}{}:
+		default:
+		}
+	}
+
+	c.mutex.Unlock()
+
 	return t, nil
 
 }
